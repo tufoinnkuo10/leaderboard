11993635e67d7b99ad8b065e85151b3a7fc48652
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-10-14 07:41:25 +0100

    reseting to use api

--- src/index.js
+++ src/index.js
@@ -1,42 +1,7 @@
 import './style.css';
 
-const scorelist = document.querySelector('.scorelist');
-const names = document.querySelector('.names');
-const numbers = document.querySelector('.numbers');
-const submitbutton = document.querySelector('#submit-button');
-const inputs = document.querySelectorAll('.names, .numbers');
-
-const saveData = (user, data) => localStorage.setItem(user, JSON.stringify(data));
-const getData = (user) => JSON.parse(localStorage.getItem(user));
-
-inputs.forEach((e) => {
-  e.addEventListener('input', () => {
-    const user = {
-      name: names.value,
-      number: numbers.value,
-    };
-    saveData('libraryData', user);
-  });
-});
-
-submitbutton.addEventListener('click', () => {
-  const scores = document.querySelector('.list');
-  scorelist.appendChild(scores);
-
-  const nom = document.createElement('li');
-  const num = document.createElement('li');
-  const Break = document.createElement('br');
-
-  const libraryData = getData('libraryData');
-  if (libraryData !== null) {
-    nom.innerHTML = libraryData.name;
-    num.innerHTML = libraryData.number;
-  }
-
-  scores.appendChild(nom);
-  scores.appendChild(num);
-  scores.appendChild(Break);
-
-  names.value = null;
-  numbers.value = null;
-});
+const userValue = document.querySelector('#name');
+const scoreValue = document.querySelector('#score');
+const points = document.querySelector('#points');
+const refresh = document.querySelector('#refresh');
+const submit = document.querySelector('#submit');
