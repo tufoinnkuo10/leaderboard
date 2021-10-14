acd985db733b443605c17e1974d8a720f58f5c79
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-10-14 10:46:25 +0100

    adding info t0 the api

--- src/myapi.js
+++ src/myapi.js
@@ -1,2 +1,15 @@
 const gameUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
 const scoresUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/H8Ha4pTxNUzGVhKJ5o9u/scores';
+
+const newGame = async () => {
+  const newGameName = 'My new game';
+  const response = await fetch(gameUrl, {
+    method: 'POST',
+    body: JSON.stringify({ name: newGameName }),
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+    },
+  });
+  const jsonData = await response.json();
+  return jsonData;
+};
